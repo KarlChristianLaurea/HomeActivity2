0beb2ba6a4c118d78194586769fe0ba53e320d3b
KarlChristianLaurea <128952675+KarlChristianLaurea@users.noreply.github.com>
2024-04-12 20:50:00 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    //retrieve form data
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    //database connection
+    $host = "localhost";
+    $dbusername = "root";
+    $dbpassword ="";
+    $dbname = "auth";
+
+    $conn = new mysqli($host,$dbusername,$dbpassword,$dbname);
+
+    if($conn->connect_error){
+        die("Connection failed: ". $conn->connect_error);
+    }
+    // validate login authentication
+    $query = "SELECT *FROM login WHERE username='$username' AND password = '$password'";
+
+    $result = $conn->query($query);
+
+    if($result->num_rows == 1){
+        //login success
+        header("Location: authorized.html");
+        exit();
+    }
+    else{
+        //login failed
+        header("Location: error.html");
+        exit();
+    }
+    $conn->close();
+}
